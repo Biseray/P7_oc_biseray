44f27fc50e2e547bfbf4ea862a42301750de0496
Biseray <allan.biseray@outlook.fr>
2023-03-29 11:09:09 +0200

    creation rating

--- kasa/src/components/Rating.jsx
+++ kasa/src/components/Rating.jsx
@@ -7,20 +7,17 @@ import starColor from "../asset/VectorStarColor.png";
 import style from "../styles/rating.module.css"
 
 const Rating = ({ RatingStar }) => {
-    const limiteStar= Array(5);
-    const Star = limiteStar.fill( <img src={starColor} alt='e'/>  )
-    const vide = limiteStar.fill(<img src={starVide} alt='e'/>)
 
-    return (
-    <div className={style.posRating}>
-    {Star.slice(5-RatingStar).map((Star, index) => {
-        return(<p key={index}> {Star} </p>)
-    })}
-            {vide.slice(RatingStar).map((vide, index) => {
-        return(<p key={index}> {vide} </p>)
-    })}
-    </div>
-  )
+
+    // return (
+    //     <div className={style.posRating}>
+    //         {RatingStar.map((index, Star) => (
+    //             <div key={index}>{Star} </div>
+               
+    //        )) }
+           
+    //     </div>
+    // )
 };
 
 export default Rating;
